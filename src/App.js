de247a944dd6b5455cdc427f7af4cb65be360990
Manuel Pérez Jurado <perezjuradomanuel@gmail.com>
2024-04-08 16:50:01 +0200

    setting project and first commit after vacations

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-
+import React from "react";
+import Navitagion from "./Navigation/Nav";
+import Products from "./Products/Products";
+import Recommended from "./Recommended/Recommended";
 function App() {
   return (
       <>
         <h1>Hello world</h1>
+        <Navitagion />
+        <Products />
+        <Recommended />
       </>
   );
 }
