984b7682345d7967b015a7d3228165e09d296195
Manuel Pérez Jurado <perezjuradomanuel@gmail.com>
2024-04-09 13:58:18 +0200

    sidebar content updated

--- src/Colors/Colors.jsx
+++ src/Colors/Colors.jsx
@@ -2,7 +2,32 @@ import React from 'react'
 
 const Colors = () => {
   return (
-    <div>Colors</div>
+    <div>
+      <label htmlFor="" className="sidebar-label-container color-title">
+          <input type="radio" name="test3" />
+          <span className='checkmark'></span>All
+        </label>
+        <label htmlFor="" className="sidebar-label-container">
+          <input type="radio" name="test3" />
+          <span className='checkmark'></span>Black
+        </label>
+        <label htmlFor="" className="sidebar-label-container">
+          <input type="radio" name="test3" />
+          <span className='checkmark'></span>Red
+        </label>
+        <label htmlFor="" className="sidebar-label-container">
+          <input type="radio" name="test3" />
+          <span className='checkmark'></span>Blue
+        </label>
+        <label htmlFor="" className="sidebar-label-container">
+          <input type="radio" name="test3" />
+          <span className='checkmark'></span>Green
+        </label>
+        <label htmlFor="" className="sidebar-label-container">
+          <input type="radio" name="test3" />
+          <span className='checkmark'></span>White
+        </label>
+    </div>
   )
 }
 
--- src/Price/Price.jsx
+++ src/Price/Price.jsx
@@ -2,7 +2,30 @@ import React from 'react'
 import "./Price.css"
 const Price = () => {
   return (
-    <div>Price</div>
+    <div className='ml'>
+      <h2 className="sidebar-title price-title">Price</h2>
+        <label htmlFor="" className="sidebar-label-container">
+          <input type="radio" name="test2" />
+          <span className='checkmark'></span>All
+        </label>
+        <label htmlFor="" className="sidebar-label-container">
+          <input type="radio" name="test2" />
+          <span className='checkmark'></span>$0 - $50
+        </label>
+        <label htmlFor="" className="sidebar-label-container">
+          <input type="radio" name="test2" />
+          <span className='checkmark'></span>$50 - $100
+        </label>
+        <label htmlFor="" className="sidebar-label-container">
+          <input type="radio" name="test2" />
+          <span className='checkmark'></span>$100 - $150
+        </label>
+        <label htmlFor="" className="sidebar-label-container">
+          <input type="radio" name="test2" />
+          <span className='checkmark'></span>Over $350
+        </label>
+    </div>
+    
   )
 }
 
